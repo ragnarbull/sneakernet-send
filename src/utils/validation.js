e7048723fc6a5c6dc74791c6bf80bdc2740a7e76
ragnarbull <joel@infisical.com>
2023-11-02 17:06:14 +1100

    Improve validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,18 +2,18 @@ class ValidationService {
   /**
    * Check the validity of a local ECDH public key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} localECDHPublicKeyJWK - the local ECDH public key JWK to validate
+   * @param {JsonWebKey} key - the local ECDH public key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidLocalECDHPublicKeyJWK(localECDHPublicKeyJWK) {
+  static isValidLocalECDHPublicKeyJWK(key) {
     if (
-      !localECDHPublicKeyJWK ||
-      typeof localECDHPublicKeyJWK !== 'object' ||
-      localECDHPublicKeyJWK.kty !== 'EC' ||
-      localECDHPublicKeyJWK.crv !== "P-521" ||
-      (localECDHPublicKeyJWK.x && localECDHPublicKeyJWK.x.length !== 88) ||
-      (localECDHPublicKeyJWK.y && localECDHPublicKeyJWK.y.length !== 88) ||
-      (localECDHPublicKeyJWK.keyOps && localECDHPublicKeyJWK.keyOps.length === 0)
+      !key ||
+      typeof key !== 'object' ||
+      key.kty !== 'EC' ||
+      key.crv !== "P-521" ||
+      (key.x && key.x.length !== 88) ||
+      (key.y && key.y.length !== 88) ||
+      (key.keyOps && key.keyOps.length === 0)
     ) {
       throw new Error('Failed to generate a valid local ECDH public key JWK');
     }
@@ -22,19 +22,19 @@ class ValidationService {
   /**
    * Check the validity of a local ECDH private key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} localECDHPrivateKeyJWK - the local ECDH private key JWK to validate
+   * @param {JsonWebKey} key - the local ECDH private key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidLocalECDHPrivateKeyJWK(localECDHPrivateKeyJWK) {
+  static isValidLocalECDHPrivateKeyJWK(key) {
     if (
-      !localECDHPrivateKeyJWK ||
-      typeof localECDHPrivateKeyJWK !== 'object' ||
-      localECDHPrivateKeyJWK.kty !== 'EC' ||
-      localECDHPrivateKeyJWK.crv !== "P-521" ||
-      (localECDHPrivateKeyJWK.x && localECDHPrivateKeyJWK.x.length !== 88) ||
-      (localECDHPrivateKeyJWK.y && localECDHPrivateKeyJWK.y.length !== 88) ||
-      (localECDHPrivateKeyJWK.d && localECDHPrivateKeyJWK.d.length !== 88) ||
-      !(localECDHPrivateKeyJWK.key_ops && localECDHPrivateKeyJWK.key_ops.includes("deriveKey"))
+      !key ||
+      typeof key !== 'object' ||
+      key.kty !== 'EC' ||
+      key.crv !== "P-521" ||
+      (key.x && key.x.length !== 88) ||
+      (key.y && key.y.length !== 88) ||
+      (key.d && key.d.length !== 88) ||
+      !(key.key_ops && key.key_ops.includes("deriveKey"))
     ) {
       throw new Error('Failed to generate a valid local ECDH private key JWK');
     }
@@ -43,18 +43,18 @@ class ValidationService {
   /**
    * Check the validity of a local ECDH private key wrapping key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} localECDHPrivateKeyWrappingKeyJWK - the local ECDH private key wrapping key JWK to validate
+   * @param {JsonWebKey} key - the local ECDH private key wrapping key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidLocalECDHPrivateKeyWrappingKeyJWK(localECDHPrivateKeyWrappingKeyJWK) {
+  static isValidLocalECDHPrivateKeyWrappingKeyJWK(key) {
     if (
-      !localECDHPrivateKeyWrappingKeyJWK ||
-      typeof localECDHPrivateKeyWrappingKeyJWK !== 'object' ||
-      localECDHPrivateKeyWrappingKeyJWK.alg !== 'A256GCM' ||
-      (localECDHPrivateKeyWrappingKeyJWK.k && localECDHPrivateKeyWrappingKeyJWK.k.length !== 43) ||
-      localECDHPrivateKeyWrappingKeyJWK.kty !== 'oct' ||
-      !localECDHPrivateKeyWrappingKeyJWK.key_ops?.includes("wrapKey") ||
-      !localECDHPrivateKeyWrappingKeyJWK.key_ops?.includes("unwrapKey")
+      !key ||
+      typeof key !== 'object' ||
+      key.alg !== 'A256GCM' ||
+      (key.k && key.k.length !== 43) ||
+      key.kty !== 'oct' ||
+      !(key.key_ops && key.key_ops.includes("wrapKey")) ||
+      !(key.key_ops && key.key_ops.includes("unwrapKey"))
     ) {
       throw new Error('Failed to generate a valid local ECDH private key wrapping key JWK');
     }
@@ -63,18 +63,18 @@ class ValidationService {
   /**
    * Check the validity of a master key wrapping key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} masterKeyWrappingKeyJWK - the master key wrapping key JWK to validate
+   * @param {JsonWebKey} key - the master key wrapping key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidMasterKeyWrappingKeyJWK(masterKeyWrappingKeyJWK) {
+  static isValidMasterKeyWrappingKeyJWK(key) {
     if (
-      !masterKeyWrappingKeyJWK ||
-      typeof masterKeyWrappingKeyJWK !== 'object' ||
-      masterKeyWrappingKeyJWK.alg !== 'A256GCM' ||
-      (masterKeyWrappingKeyJWK.k && masterKeyWrappingKeyJWK.k.length !== 43) ||
-      masterKeyWrappingKeyJWK.kty !== 'oct' ||
-      !(masterKeyWrappingKeyJWK.key_ops && masterKeyWrappingKeyJWK.key_ops.includes("wrapKey")) ||
-      !(masterKeyWrappingKeyJWK.key_ops && masterKeyWrappingKeyJWK.key_ops.includes("unwrapKey"))
+      !key ||
+      typeof key !== 'object' ||
+      key.alg !== 'A256GCM' ||
+      (key.k && key.k.length !== 43) ||
+      key.kty !== 'oct' ||
+      !(key.key_ops && key.key_ops.includes("wrapKey")) ||
+      !(key.key_ops && key.key_ops.includes("unwrapKey"))
     ) {
       throw new Error('Failed to generate a valid master key wrapping key AES256GCM JWK');
     }
@@ -83,14 +83,14 @@ class ValidationService {
   /**
    * Check the validity of a wrapped local ECDH private key JSON Web Key (JWK).
    * 
-   * @param {ArrayBuffer} wrappedLocalECDHPrivateKeyJWK - the wrapped local ECDH private key JWK to validate
+   * @param {ArrayBuffer} wrappedKey - the wrapped local ECDH private key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidWrappedLocalECDHPrivateKeyJWK(wrappedLocalECDHPrivateKeyJWK) {
+  static isValidWrappedLocalECDHPrivateKeyJWK(wrappedKey) {
     if (
-      !wrappedLocalECDHPrivateKeyJWK ||
-      !(wrappedLocalECDHPrivateKeyJWK instanceof ArrayBuffer) ||
-      wrappedLocalECDHPrivateKeyJWK.byteLength !== 362
+      !wrappedKey ||
+      !(wrappedKey instanceof ArrayBuffer) ||
+      wrappedKey.byteLength !== 362
     ) {
       throw new Error('Failed to wrap the local ECDH private key JWK');
     }
@@ -99,14 +99,14 @@ class ValidationService {
   /**
    * Check the validity of a wrapped master key.
    * 
-   * @param {ArrayBuffer} wrappedMasterKeyJWK - the wrapped master key to validate
+   * @param {ArrayBuffer} wrappedKey - the wrapped master key to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidWrappedMasterKey(wrappedMasterKeyJWK) {
+  static isValidWrappedMasterKey(wrappedKey) {
     if (
-      !wrappedMasterKeyJWK ||
-      !(wrappedMasterKeyJWK instanceof ArrayBuffer) ||
-      wrappedMasterKeyJWK.byteLength !== 138
+      !wrappedKey ||
+      !(wrappedKey instanceof ArrayBuffer) ||
+      wrappedKey.byteLength !== 138
     ) {
       throw new Error('Failed to wrap the master key');
     }
@@ -115,18 +115,18 @@ class ValidationService {
   /**
    * Check the validity of a master ECDH public key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} masterECDHPublicKeyJWK - the master ECDH public key JWK to validate
+   * @param {JsonWebKey} key - the master ECDH public key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidMasterECDHPublicKeyJWK(masterECDHPublicKeyJWK) {
+  static isValidMasterECDHPublicKeyJWK(key) {
     if (
-      !masterECDHPublicKeyJWK ||
-      typeof masterECDHPublicKeyJWK !== 'object' ||
-      masterECDHPublicKeyJWK.kty !== 'EC' ||
-      masterECDHPublicKeyJWK.crv !== "P-521" ||
-      (masterECDHPublicKeyJWK.x && masterECDHPublicKeyJWK.x.length !== 88) ||
-      (masterECDHPublicKeyJWK.y && masterECDHPublicKeyJWK.y.length !== 88) ||
-      (masterECDHPublicKeyJWK.key_ops && masterECDHPublicKeyJWK.key_ops.length !== 0)
+      !key ||
+      typeof key !== 'object' ||
+      key.kty !== 'EC' ||
+      key.crv !== "P-521" ||
+      (key.x && key.x.length !== 88) ||
+      (key.y && key.y.length !== 88) ||
+      (key.key_ops && key.key_ops.length !== 0)
     ) {
       throw new Error('The master ECDH public key is not a valid EC P-521 JWK');
     }
@@ -135,19 +135,19 @@ class ValidationService {
   /**
    * Check the validity of a master ECDH private key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} masterECDHPrivateKeyJWK - the master ECDH private key JWK to validate
+   * @param {JsonWebKey} key - the master ECDH private key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidMasterECDHPrivateKeyJWK(masterECDHPrivateKeyJWK) {
+  static isValidMasterECDHPrivateKeyJWK(key) {
     if (
-      !masterECDHPrivateKeyJWK ||
-      typeof masterECDHPrivateKeyJWK !== 'object' ||
-      masterECDHPrivateKeyJWK.kty !== 'EC' ||
-      masterECDHPrivateKeyJWK.crv !== "P-521" ||
-      (masterECDHPrivateKeyJWK.x && masterECDHPrivateKeyJWK.x.length !== 88) ||
-      (masterECDHPrivateKeyJWK.y && masterECDHPrivateKeyJWK.y.length !== 88) ||
-      (masterECDHPrivateKeyJWK.d && masterECDHPrivateKeyJWK.d.length !== 88) ||
-      !(masterECDHPrivateKeyJWK.key_ops && masterECDHPrivateKeyJWK.key_ops.includes("deriveKey"))
+      !key ||
+      typeof key !== 'object' ||
+      key.kty !== 'EC' ||
+      key.crv !== "P-521" ||
+      (key.x && key.x.length !== 88) ||
+      (key.y && key.y.length !== 88) ||
+      (key.d && key.d.length !== 88) ||
+      !(key.key_ops && key.key_ops.includes("deriveKey"))
     ) {
       throw new Error('The master ECDH private key is not a valid EC P-521 JWK');
     }
@@ -156,18 +156,18 @@ class ValidationService {
   /**
    * Check the validity of a master key JSON Web Key (JWK).
    * 
-   * @param {JsonWebKey} masterKeyJWK - the master key JWK to validate
+   * @param {JsonWebKey} key - the master key JWK to validate
    * @throws {Error} - Throws an error if the key is not valid.
    */
-  static isValidMasterKeyJWK({ masterKeyJWK }) {
+  static isValidMasterKeyJWK(key) {
     if (
-      !masterKeyJWK ||
-      typeof masterKeyJWK !== 'object' ||
-      masterKeyJWK.alg !== 'A256GCM' ||
-      (masterKeyJWK.k && masterKeyJWK.k.length !== 43) ||
-      masterKeyJWK.kty !== 'oct' ||
-      !(masterKeyJWK.key_ops && masterKeyJWK.key_ops.includes("encrypt")) ||
-      !(masterKeyJWK.key_ops && masterKeyJWK.key_ops.includes("decrypt"))
+      !key ||
+      typeof key !== 'object' ||
+      key.alg !== 'A256GCM' ||
+      (key.k && key.k.length !== 43) ||
+      key.kty !== 'oct' ||
+      !(key.key_ops && key.key_ops.includes("encrypt")) ||
+      !(key.key_ops && key.key_ops.includes("decrypt"))
     ) {
       throw new Error('The master key is not a valid AES256GCM JWK');
     }
